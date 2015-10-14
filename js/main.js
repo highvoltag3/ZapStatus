885f883955b79b23f1a1061fd004c62f85c32cbb
Dario J Novoa V <dnovoa@ziprealty.com>
2015-10-14 16:20:43 -0700

    Remember auth, reports and nav
    - Added a nav
    - Added a report that loads today statuses
    - Added logout
    - Cleaned up the UI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+require.config({
+    paths: {
+        'jQuery': 'jquery.min',
+    },
+    shim: {
+        'jQuery': {
+            exports: '$'
+        }
+    }
+});
+
+
 var zapstatus = new Firebase("https://zapstatus.firebaseio.com");
 
 var USERNAME_CONSTANT = "";
@@ -17,10 +29,11 @@ var login = function() {
     } else {
       console.log("Authenticated successfully with payload:", authData);
       $('#login').fadeOut();
-      $('#status').fadeIn();
+      $('#status, #report').fadeIn();
       USERNAME_CONSTANT = username;
       getUser(username);
       getYesterdaysInfo(username);
+      report();
     }
   });
 
@@ -79,39 +92,95 @@ var saveStatus = function (username) {
       today = $('#today').val(),
       blockers = $('#blockers').val();
 
+  var currentDate = (function () {
+        var fullDate = new Date();console.log(fullDate);
+        var twoDigitMonth = fullDate.getMonth()+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
+        var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
+        var currentDate = twoDigitDate + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+
+        return currentDate;
+      })();
+
   usersRef.set({
     "status" : {
       "blocker" : blockers,
       "today" : today,
-      "yesterday" : yesterday
+      "yesterday" : yesterday,
+      "date": currentDate
     }
   });
 };
 
-var register = function (username) {
-  var usersRef = zapstatus.child("users/" + username);
-  var name = $('#firstname').val(),
-      email = $('#email').val(),
-      lastname = $('#lastname').val(),
-      password = $('#password').val();
+// var register = function (username) {
+//   var usersRef = zapstatus.child("users/" + username);
+//   var name = $('#firstname').val(),
+//       email = $('#email').val(),
+//       lastname = $('#lastname').val(),
+//       password = $('#password').val();
 
-  usersRef.set({
-    "info" : {
-      "name" : name,
-      "lastname" : lastname,
-      "email": email
-    }
+//   usersRef.set({
+//     "info" : {
+//       "name" : name,
+//       "lastname" : lastname,
+//       "email": email
+//     }
+//   });
+// };
+
+
+var report = function() {
+
+  require(["text!../templates/status.handlebars", 'underscore', "jQuery"], function(statusTpl, _) {
+
+    var createStatus =  function(placeholderId, data) {
+      var deferred = $.Deferred();
+      var source  = statusTpl,
+      template    = Handlebars.compile(source),
+      placeholder = $('#' + placeholderId);
+
+      
+      deferred.resolve( placeholder.append(template(data)).hide().fadeIn('slow') );
+
+      return deferred.promise();
+    };
+
+
+    zapstatus.on("value", function(snapshot) {
+      console.log(snapshot.val());
+
+      var data = snapshot.val();
+
+      var today = (function () {
+        var fullDate = new Date();console.log(fullDate);
+        var twoDigitMonth = fullDate.getMonth()+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
+        var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
+        var currentDate = twoDigitDate + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+
+        return currentDate;
+        //$('.today').text(currentDate);
+      })();
+
+      
+      $("#report-content").empty();
+
+      _.each( data.users, function( val, key ) {
+        console.log(val.status.date == today, "IF IS");
+        val.user = key;
+        if( val.status.date == today ) {
+          createStatus("report-content", val).done(function(){
+            $('.today').text(today);
+          });
+        }
+      });
+
+      
+      
+    });
+
+          
+
   });
 };
-
-var today = (function () {
-  var fullDate = new Date();console.log(fullDate);
-  var twoDigitMonth = fullDate.getMonth()+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
-  var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
-  var currentDate = twoDigitDate + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-
-  $('#today').text(currentDate);
-})();
 
 
 
@@ -122,4 +191,28 @@ $("#login-btn").on('click', function(e) {
 
 $("#btnSubmit").on('click', function(e) {
   saveStatus(USERNAME_CONSTANT);
+});
+
+//check if I'm logged in if I am let me in!
+$(function () {
+  var isAuth = zapstatus.getAuth();
+  
+  if(isAuth) {
+    console.log(isAuth);
+    var _email = isAuth.password.email,
+        _username = _email.substring(0, _email.indexOf('@'));
+
+    $('#login').fadeOut();
+    $('#status, #report').fadeIn();
+    USERNAME_CONSTANT = _username;
+    getUser(_username);
+    getYesterdaysInfo(_username);
+    report();
+  }
+
+  $("#logout-btn").on('click', function(e) {
+    // Unauthenticate the client
+    zapstatus.unauth();
+    window.location.reload();
+  });
 });
