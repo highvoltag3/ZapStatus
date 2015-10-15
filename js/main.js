c539ffd5584680aa4d283a1e6de7783a10aca843
Dario J Novoa V <dnovoa@ziprealty.com>
2015-10-14 18:38:01 -0700

    minor presentation enhancements
    Minot visual changes

--- js/main.js
+++ js/main.js
@@ -201,6 +201,14 @@ var report = function() {
     zapstatus.on("value", function(snapshot) {
       console.log(snapshot.val());
 
+      var today = (function () {
+        var fullDate = new Date();console.log(fullDate);
+        var twoDigitMonth = fullDate.getMonth()+"";if(twoDigitMonth.length==1)  twoDigitMonth="0" +twoDigitMonth;
+        var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1) twoDigitDate="0" +twoDigitDate;
+        var currentDate = twoDigitDate + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+
+        return currentDate;
+      })();
       var data = snapshot.val();
       
       $("#report-content").empty();
